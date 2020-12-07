3b3bb0c80486b876bd192a5c05357844d6811246
SKYul <folimworld@gmail.com>
2020-12-07 14:59:15 +0900

    Update osinfo-db.rb

--- osinfo-db.rb
+++ osinfo-db.rb
@@ -1,8 +1,8 @@
 class OsinfoDb < Formula
   desc "Libosinfo database files"
   homepage "https://libosinfo.org/"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-20191125.tar.xz"
-  sha256 "e8f4e8c3e3ee738191511a4549fc8e4895b501312f2a7d383d5bde72a76f5580"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-20201119.tar.xz"
+  sha256 "74f01379359248cb253f86d1126fffc89f319136e35d483c1fd4aa01b3200ca8"
 
   depends_on "osinfo-db-tools" => :build
 
